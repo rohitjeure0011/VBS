e88ad62276d5abb3d4ce1667af2a0c38f65086a9
rohitjeure0011 <rohitjeure0011@gmail.com>
2025-12-30 18:59:49 +0530

    First Commit

---
+++ src/main/java/com/vbs/demo/controller/TransactionController.java
@@ -0,0 +1,116 @@
+package com.vbs.demo.controller;
+
+
+import com.vbs.demo.Dto.TransactionDto;
+import com.vbs.demo.Dto.TransferDto;
+import com.vbs.demo.models.Transaction;
+import com.vbs.demo.models.User;
+import com.vbs.demo.repositiories.TransactionRepo;
+import com.vbs.demo.repositiories.UserRepo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RestController
+@CrossOrigin(origins = "*")
+public class TransactionController {
+    @Autowired
+    UserRepo userRepo;
+    @Autowired
+    TransactionRepo transactionRepo;
+
+    @PostMapping("/deposit")
+    public String deposit(@RequestBody TransactionDto obj)
+    {
+        User user = userRepo.findById(obj.getId())
+                .orElseThrow(()->new RuntimeException("not found"));
+        double newBalance = user.getBalance() + obj.getAmount();
+        user.setBalance(newBalance);
+        userRepo.save(user);
+        Transaction t = new Transaction();
+        t.setAmount(obj.getAmount());
+        t.setCurrBalance(newBalance);
+        t.setDescription("Rs" + obj.getAmount() + "Deposit Successful" );
+        t.setUserId(obj.getId());
+        transactionRepo.save(t);
+        return "Deposit Successful";
+
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestBody TransactionDto obj)
+    {
+        User user = userRepo.findById(obj.getId())
+                .orElseThrow(()->new RuntimeException("not found"));
+        double newBalance = user.getBalance() - obj.getAmount();
+        if (newBalance<0)
+        {
+            return "Balance Insufficient";
+        }
+        user.setBalance(newBalance);
+        userRepo.save(user);
+        Transaction t = new Transaction();
+        t.setAmount(obj.getAmount());
+        t.setCurrBalance(newBalance);
+        t.setDescription("Rs" + obj.getAmount() + "Withdrawal Successful" );
+        t.setUserId(obj.getId());
+        transactionRepo.save(t);
+        return "Withdrawal Successful";
+
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(@RequestBody TransferDto obj)
+    {
+        User sender = userRepo.findById(obj.getId())
+                .orElseThrow(()->new RuntimeException("Not found"));
+        User rec = userRepo.findByUsername(obj.getUsername());
+
+        if(rec==null) return "Username Not Found";
+        if(sender.getId()==rec.getId()) return "Self Transaction Not Allowed";
+        if(obj.getAmount()<1) return "Invalid Amount";
+        double sbalance = sender.getBalance() - obj.getAmount();
+        double rbalance = rec.getBalance() + obj.getAmount();
+
+        if (sbalance<0) return "Insuffient Balance";
+        sender.setBalance(sbalance);
+        rec.setBalance(rbalance);
+
+        userRepo.save(sender);
+        userRepo.save(rec);
+
+        Transaction t1 = new Transaction();
+        Transaction t2 = new Transaction();
+
+        t1.setAmount(obj.getAmount());
+        t1.setCurrBalance(sbalance);
+        t1.setDescription("Rs" + obj.getAmount() + "sent to user" + rec.getUsername() );
+        t1.setUserId(sender.getId());
+
+        t2.setAmount(obj.getAmount());
+        t2.setCurrBalance(rbalance);
+        t2.setDescription("Rs" + obj.getAmount() + "Received from user" + sender.getUsername() );
+        t2.setUserId(rec.getId());
+
+        transactionRepo.save(t1);
+        transactionRepo.save(t2);
+        return "Transfer Done Succesfully";
+
+
+    }
+    @GetMapping("/passbook/{id}")
+    public List<Transaction> getPassbook(@PathVariable int id)
+    {
+        return transactionRepo.findAllByUserId(id);
+    }
+
+}
+
+
